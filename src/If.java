4d06c8f5060dd22ad1385ad06064617791fc0dec
Irene Ruiz del portal <irene.ruizdelportal@gmail.com>
2023-03-09 20:32:20 +0100

    Initial commit

---
+++ src/If.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroIf = -10;
+        if (numeroIf > 0) {
+            System.out.print("Este numero es positivo");
+        } else if (numeroIf == 0) {
+            System.out.print("Este numero es cero");
+        }
+        else {
+            System.out.print("Este número es negativo");
+        }
+
+    }
+}
